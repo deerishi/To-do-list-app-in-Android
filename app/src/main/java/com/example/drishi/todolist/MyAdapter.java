db02723c80dd492aca166b34072e3337c2aa9ec7
Deepak Rishi <deerishi@gmail.com>
2017-07-06 00:06:31 -0400

    Add image icon and item number

--- app/src/main/java/com/example/drishi/todolist/MyAdapter.java
+++ app/src/main/java/com/example/drishi/todolist/MyAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -27,6 +28,12 @@ public class MyAdapter extends ArrayAdapter<String> {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view = inflater.inflate(R.layout.custom_list_layout, null);
         TextView tv=(TextView)view.findViewById(R.id.serial_num);
+        ImageView img=(ImageView) view.findViewById(R.id.icon);
+        if(data.get(position).length()>5){
+            img.setImageResource(R.drawable.icon1);
+        }else{
+            img.setImageResource(R.drawable.icon2);
+        }
         tv.setText(Integer.toString(position+1)+") "+data.get(position));
         return view;
     }
